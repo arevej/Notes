73bc801a3ad32455e01505808e71bc655f0a112d
Tim Polozkov <tim@Tims-MacBook-Air.local>
2017-05-17 19:15:20 +0300

    Initial commit

---
+++ authentication/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+
+def index (request):
+    if request.user.is_authenticated():
+        return render(request, 'notes/notes.html' )
+    else:
+        return HttpResponseRedirect(reverse('login'))
+
+def register (request):
+    if request.method == 'GET':
+        return render(request, 'registration/register.html', {})
+    else:
+        login_ = request.POST['login']
+        email = request.POST['email']
+        password = request.POST['password']
+        try:
+            User.objects.create_user(login_, email, password)
+            user=authenticate(request, username=login_, password=password)
+            login(request, user)
+            return HttpResponseRedirect(reverse('notes'))
+        except Exception as e:
+            return render(request, 'registration/register.html', { 'error': str(e) })
